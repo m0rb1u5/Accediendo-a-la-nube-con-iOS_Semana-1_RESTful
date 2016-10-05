333d2ef51d6dcefdb2326eb0ce35c0b7805c8ff0
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-10-05 03:45:37 -0500

    Clase "Accediendo a los recursos de la Web con iOS".

--- Accediendo-a-la-nube-con-iOS_Semana-1_RESTful/ViewController.swift
+++ Accediendo-a-la-nube-con-iOS_Semana-1_RESTful/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        asincrono()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func sincrono() {
+        let urls = "http://dia.ccm.itesm.mx"
+        let url: NSURL? = NSURL(string: urls)
+        let datos: NSData? = NSData(contentsOf: url! as URL)
+        let texto: NSString? = NSString(data: datos! as Data, encoding: String.Encoding.utf8.rawValue)
+        print(texto!)
+    }
+    func asincrono() {
+        let urls = "http://dia.ccm.itesm.mx"
+        let url: NSURL? = NSURL(string: urls)
+        let sesion: URLSession = URLSession.shared
+        let bloque = { (datos: Data?, resp: URLResponse?, error: Error?) -> Void in
+            let texto: NSString? = NSString(data: datos! as Data, encoding: String.Encoding.utf8.rawValue)
+            print(texto!)
+        }
+        
+        let dt: URLSessionDataTask = sesion.dataTask(with: url! as URL, completionHandler: bloque)
+        dt.resume()
+        print("antes o después?")
+    }
 }
 
